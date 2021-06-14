abc9dabed3939a42e665b67a636fbaedba7430a8
Peter Robert <pwambui93@gmail.com>
2021-06-14 18:57:49 +0300

    Intergrate the database and update the routing

--- routes/genres.js
+++ routes/genres.js
@@ -26,56 +26,73 @@ const Genre = mongoose.model('Genre', genreSchema);
 
 router.get('/', (req, res) => {
     Genre.find().then((results) => {
-        if(results.length < 1){return res.send('There are no genres yet.')}
+        if (results.length < 1) { return res.send('There are no genres yet.') }
         res.status(200).send(results)
     }).catch((err) => console.log(err));
-   
+
 })
 
 router.get('/:id', (req, res) => {
-    const genreId = parseInt(req.params.id);
-    const genre = genres.find(value => value.id === genreId);
-    if (!genre) { return res.status(404).send('There is no genre with that specific id') };
-    res.status(200).send(genre);
+    const genreId = req.params.id;
+    Genre.findById(genreId).then((genre) => {
+        res.status(200).send(genre);
+    }).catch((err) => {
+        console.log(err)
+        res.status(404).send('There is no genre with that specific id')
+    });
 });
 
 router.post('/', (req, res) => {
+    // === Create new genre
+    const createGenre = (obj) => {
+        let data = new Genre(obj);
+        data.save().then((results) => {
+            res.status(200).send(results)
+        }).catch((err) => { console.log(err) })
+    }
+
+    // === Validate the user request
     const responseValidation = objValidation(req.body);
     responseValidation.then((value) => {
-        let data = new Genre(value);
-        data.save().then((results) => {   
-            res.status(200).send(results)
-        })  
+        createGenre(value);
     }).catch((err) => {
         res.status(404).send(err.details[0].message)
     })
 })
 
 router.put('/:id', (req, res) => {
-    let genreId = parseInt(req.params.id);
+    let genreId = req.params.id;
+    // === Update function
+    const updateFunction = (obj) => {
+        Genre.findByIdAndUpdate(genreId, obj, { new: true })
+            .then((data) => {
+                res.status(200).send(data)
+            }).catch((err) => {
+                res.status(404).send('There is no genre with that specific id');
+                console.log(err)
+            })
+    }
 
-    let genre = genres.find((value) =>  value.id === genreId );
-    if (!genre) { return res.status(404).send('There is no genre with that specific id') };
-
-    // validation
+    // ===== validation
     let results = objValidation(req.body);
     results.then((value) => {
-       genre.title = value.title;
-       res.status(200).send(genre)
+        updateFunction(value)
     }).catch((err) => {
         res.status(404).send(err.details[0].message)
     })
 
 })
 
-router.delete('/:id', (req,res) => {
-   let genreId = parseInt(req.params.id);
-   let genre = genres.find((value) =>  value.id === genreId );
-   if (!genre) { return res.status(404).send('There is no genre with that specific id') };
-
-   let removeItem = genreId - 1;
-   let ans =  genres.splice(removeItem, 1);
-   res.status(200).send(ans)
+router.delete('/:id', (req, res) => {
+    let genreId = req.params.id;
+    Genre.deleteOne({ _id: genreId })
+        .then((data) => {
+            res.status(200).send('Genre deleted successfuly..')
+            console.log(data)
+        }).catch((err) => {
+            res.status(404).send('There is no genre with that specific id')
+            console.log(err)
+        })
 
 })
 
